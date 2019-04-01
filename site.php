72de9c3d76a50c53d5d2a7a08fde675c4e4f3fe7
Jeann <jeannguilherme@yahoo.com.br>
2019-04-01 19:49:36 -0300

    rota order dando erro

--- site.php
+++ site.php
@@ -6,6 +6,8 @@ use \Hcode\Model\Category;
 use \Hcode\Model\Cart;
 use \Hcode\Model\Address;
 use \Hcode\Model\User;
+use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
 
 $app->get('/', function(){
 
@@ -191,7 +193,7 @@ $app->post("/checkout", function(){
 		exit;
 	}
 
-	if(!isset($_POST['desditrict']) || $_POST['desditrict'] === ''){
+	if(!isset($_POST['desdistrict']) || $_POST['desdistrict'] === ''){
 		Address::setMsgError("Informe o bairro.");
 		header("Location: /checkout");
 		exit;
@@ -226,7 +228,23 @@ $app->post("/checkout", function(){
 
 	$address->save();
 
-	header("Location: /order");
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getCalculateTotal();
+
+	$order = new Order();
+
+	$order->setData([
+		'idcart'=>$cart->getidcart(),
+		'idaddress'=>$address->getidaddress(),
+		'iduser'=>$user->getiduser(),
+		'idstatus'=>OrderStatus::EM_ABERTO,
+		'vltotal'=>$totals['vlprice'] + $cart->getvlfreight()
+	]);
+
+	$order->save();
+
+	header("Location: /order".$order->getidorder());
 	exit;
 
 });
@@ -439,6 +457,22 @@ $app->post("/profile", function(){
 
 	header('Location: /profile');
 	exit;
+
+});
+
+$app->get("/order/:idorder", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$page = new Page();
+
+	$page->setTpl("payment", [
+		'order'=>$order->getValues()
+	]);
 
 });
 
--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -51,7 +51,7 @@ class Address extends Model{
 
 		$sql = new Sql();
 
-		$resturn = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
+		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
 			'idaddress'=>$this->getidaddress(),
 			'idperson'=>$this->getidperson(),
 			'desaddress'=>utf8_decode($this->getdesaddress()),
